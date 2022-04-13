64bd8dd79a2210dc97c55928e64ca6472fedd4cd
snkvch <julia.sankevich8@gmail.com>
2022-04-13 12:57:46 +0300

    Remove LogBox

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
-import { LogBox } from 'react-native';
 
 import MoviesNavigator from './src/navigation/MoviesNavigator';
 import store from './src/redux/store';
 
 function App() {
-  LogBox.ignoreAllLogs();
   return (
     <Provider store={store}>
       <ThemeProvider>
