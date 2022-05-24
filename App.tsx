487e5a293fc64b6c0134bae69b1f77416846f743
snkvch <julia.sankevich8@gmail.com>
2022-05-24 18:05:13 +0300

    Add redux-persist

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import AppNavigator from './src/navigation/AppNavigator';
-import store from './src/redux/store';
+import reduxStore from './src/redux/store';
 
 function App() {
+  const { store, persistor } = reduxStore;
   return (
     <Provider store={store}>
-      <ThemeProvider>
-        <AppNavigator />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <AppNavigator />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import Flipper from 'redux-flipper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
@@ -8,16 +10,26 @@ import rootSaga from './rootSaga';
 const sagaMiddleware = createSagaMiddleware();
 const middlewareToApply = [sagaMiddleware];
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['movies', 'details', 'user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 if (__DEV__) {
   const createDebugger = Flipper;
   middlewareToApply.push(createDebugger() as never);
 }
 
 const middleware = applyMiddleware(...middlewareToApply);
-const store = createStore(rootReducer, middleware);
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
+
 sagaMiddleware.run(rootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export default { store, persistor };
